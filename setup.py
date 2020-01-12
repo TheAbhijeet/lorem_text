9b2936733c393b0c130c79b0d8c4fa55c9b3be9f
TheAbhijeet <abhijeetpal321@gmail.com>
2020-01-12 22:50:56 +0530

    updates

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/TheAbhijeet/lorem-text',
-    version='0.1.0',
+    version='1.0',
     zip_safe=False,
 )
