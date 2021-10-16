57cc4247a2315aa00e34489f754767e719b90747
Abhijeet <abhijeet.pal@capitalnumbers.com>
2021-10-16 11:12:35 +0530

    Version update

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Lorem Text'
-copyright = "2020, Abhijeet Pal"
+copyright = "2021, Abhijeet Pal"
 author = "Abhijeet Pal"
 
 # The version info for the project you're documenting, acts as replacement
